604be26af8a86cc38ef9e7a2c26bcffc2a37d37d
Amirul Haqe <amiruldev1@gmail.com>
2021-07-25 19:59:29 +0530

    drafted html layout and css

--- todo.py
+++ todo.py
@@ -4,13 +4,23 @@ from . import db
 bp = Blueprint('todo', 'todo', url_prefix='/')
 
 @bp.route('/')
-def todos():
+def index():
 	#conn = db.get_db
 	#cursor = conn.cursor()
 	#cursor.execute('select title, company_name, jd_text from openings')
 	#jobs = cursor.fetchall()
+	return render_template('todo/index.html')
+
+@bp.route('/signin')       
+def signin():
+	return render_template('todo/signin.html')
+
+@bp.route('/signup')       
+def signup():
+	return render_template('todo/signup.html')
+
+@bp.route('/todos/')       
+def todos():
 	return render_template('todo/todos.html')
 
-@bp.route('/<id>')        # <> to take whatever comes in 
-def detail(id):
-	return render_template('todo/tododetails.html', id=id)
+
