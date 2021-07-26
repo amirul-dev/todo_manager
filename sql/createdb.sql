71ed8e0031a926457c56052f4a235685d43011ac
Amirul Haqe <amiruldev1@gmail.com>
2021-07-26 10:55:49 +0530

    signin and signup updated

--- sql/createdb.sql
+++ sql/createdb.sql
@@ -1,6 +1,17 @@
 drop table if exists todo;
 
+drop table if exists users;
+
 create table todo (
-       id serial primary key,
-       title text,
-       description text);
+       id integer primary key AUTOINCREMENT not null,
+       title text not null,
+       description text,
+       due_time timestamp);
+
+create table users (
+       id integer primary key AUTOINCREMENT not null,
+       name text not null,
+       email text not null,
+       password text not null);
+
+insert into users (name, email, password) values ('amirul', 'amirul@mail.com', '12345');
--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, render_template
+from flask import Blueprint, g, render_template, request, redirect, url_for
 from . import db
 
 bp = Blueprint('todo', 'todo', url_prefix='/')
@@ -11,13 +11,52 @@ def index():
 	#jobs = cursor.fetchall()
 	return render_template('todo/index.html')
 
-@bp.route('/signin')       
+@bp.route('/signin', methods=['GET', 'POST'])       
 def signin():
-	return render_template('todo/signin.html')
-
-@bp.route('/signup')       
+    conn = db.get_db()
+    cursor = conn.cursor()
+    if request.method == "GET":
+        return render_template('todo/signin.html')
+    elif request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = request.form.get('remember')
+        cursor.execute("select email from users")
+        emails = cursor.fetchall()
+        emails = [x[0] for x in emails]
+        if email in emails:
+                cursor.execute("select password from users where email = ?", [email])
+                db_password = cursor.fetchone()
+                if password == db_password[0]:
+	                return redirect(url_for("todo.todos"), 302)
+                else:
+                        return render_template('todo/signin.html') 
+        else:
+                return redirect(url_for("todo.signup"), 302) 
+        
+@bp.route('/signup', methods=['GET', 'POST'])       
 def signup():
-	return render_template('todo/signup.html')
+    conn = db.get_db()
+    cursor = conn.cursor()
+    if request.method == "GET":
+        return render_template('todo/signup.html')
+    elif request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        remember = request.form.get('remember')
+        cursor.execute("select email from users")
+        emails = cursor.fetchall()
+        emails = [x[0] for x in emails]
+        if email in emails:
+                return redirect(url_for("todo.signin"), 302)
+        elif password!=password2: 
+                return render_template('todo/signup.html') 
+        else:
+                cursor.execute("insert into users (name, email, password) values (?,?,?)", [name, email, password])
+                conn.commit()
+                return redirect(url_for("todo.todos"), 302)
 
 @bp.route('/todos/')       
 def todos():
