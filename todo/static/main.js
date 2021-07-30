f16dceada70a46e2a5a8365e2fa876f0a96684a1
Amirul Haqe <amiruldev1@gmail.com>
2021-07-30 22:05:03 +0530

    more jquery features added

--- todo/static/main.js
+++ todo/static/main.js
@@ -1,15 +1,26 @@
+function first_todo(userid){
+        $.get('/firstitem/'+userid, function(resp){
+	data = resp.split(';');
+	console.log(resp);
+	$('.first-todo h2').html(data[0]);
+	$('.first-todo h5').html(data[1]);
+	});
+}
+
 function deletebutton(){
 	console.log('loaded')
 	$(".todo-list, .shopping-list").on("click", ".delete-btn", function() {
 	console.log('delete btn clicked');
 	link = $(this).parent().attr('action');
-	console.log(link);
 	$(this).closest("li").remove();
 	event.preventDefault();
 	$.post(link);
 	if ($('.todo-list li').length==0){
 		$('.first-todo').remove();
 	}
+	console.log(link[link.length-1]);
+	userid=link.split('/')[4];
+	first_todo(userid);
 });		
 }
 
@@ -20,11 +31,16 @@ function tick(){
 	if ($(this).is(':checked')){
 		link=link+'checked';
 		$(this).parent().css('text-decoration','line-through');
+		$(this).parent().next().html('');
 	} else {
 		link=link+'none';
 		$(this).parent().css('text-decoration','');
+		$(this).parent().next().html('Overdue');
 	}
 	$.post(link);
+	userid=link.split('/')[4];
+	console.log(userid);
+	first_todo(userid);
 });		
 }
 
--- todo/todo.py
+++ todo/todo.py
@@ -9,35 +9,41 @@ username=''
 bp = Blueprint('todo', 'todo', url_prefix='/')
 
 def rem_time_calc(f):
-	due_date = datetime.datetime.strptime(f[2], '%Y-%m-%d')
-	dt = f[3].split(':')
-	due_time = datetime.timedelta(hours=int(dt[0]), minutes=int(dt[1]))
-	due_datetime = due_date+due_time
-	current_datetime = datetime.datetime.now()
-	rem_time = due_datetime - current_datetime
-	days = abs(rem_time.days)
-	seconds = rem_time.seconds
-	hours = seconds//3600
-	minutes = (seconds//60)%60
-	if days==0:
-		rem_time = f'{hours}hrs, {minutes}mins'
+	if f[4]=='checked':
+		return '', 'Done'
 	else:
-		rem_time = f'{days}days, {hours}hrs, {minutes}mins'
-	if due_datetime > current_datetime:
-		status = 'Due'
-	else:
-		status = 'Overdue'
-	return rem_time, status
+		due_date = datetime.datetime.strptime(f[2], '%Y-%m-%d')
+		dt = f[3].split(':')
+		due_time = datetime.timedelta(hours=int(dt[0]), minutes=int(dt[1]))
+		due_datetime = due_date+due_time
+		current_datetime = datetime.datetime.now()
+		rem_time = due_datetime - current_datetime
+		days = abs(rem_time.days)
+		seconds = rem_time.seconds
+		hours = seconds//3600
+		minutes = (seconds//60)%60
+		if days==0:
+			rem_time = f'{hours}hrs, {minutes}mins'
+		else:
+			rem_time = f'{days}days, {hours}hrs, {minutes}mins'
+		if due_datetime > current_datetime:
+			status = 'Due'
+		else:
+			status = 'Overdue'
+		return ' by : '+rem_time, status
 
-def check_overdue(due_date, due_time):
-	due_date = datetime.datetime.strptime(due_date, '%Y-%m-%d')
-	dt = due_time.split(':')
-	due_time = datetime.timedelta(hours=int(dt[0]), minutes=int(dt[1]))
-	due_datetime = due_date+due_time
-	current_datetime = datetime.datetime.now()
-	if due_datetime < current_datetime:
-		status = 'Overdue'
-		return status
+def check_overdue(due_date, due_time,status):
+	if status=='checked':
+		return ''
+	else:
+		due_date = datetime.datetime.strptime(due_date, '%Y-%m-%d')
+		dt = due_time.split(':')
+		due_time = datetime.timedelta(hours=int(dt[0]), minutes=int(dt[1]))
+		due_datetime = due_date+due_time
+		current_datetime = datetime.datetime.now()
+		if due_datetime < current_datetime:
+			status = 'Overdue'
+			return status
 	
 def format_date(d):
 	d = datetime.datetime.strptime(str(d), '%Y-%m-%d')
@@ -130,6 +136,20 @@ def todos(userid):
         cursor.execute("insert into todos (title, due_date, due_time, userid) values (?,?,?,?)", [newtodo, newtodo_date, newtodo_time, userid])
         conn.commit()
         return redirect(url_for("todo.todos", userid=userid), 302)
+
+@bp.route('/firstitem/<userid>')       
+def firstitem(userid):
+    conn = db.get_db()
+    cursor = conn.cursor()
+    cursor.execute("select id, title, due_date, due_time, status from todos where userid=? order by due_date, due_time ",[userid])
+    data = cursor.fetchone()
+    if data : 
+        first_todo = data[1]
+        rem_time, status = rem_time_calc(data)
+    else:
+        first_todo = ''
+        rem_time, status = '',''
+    return f'{first_todo};{status}{rem_time}'
 
 @bp.route('/shopping/<userid>', methods=['GET', 'POST'])       
 def shopping(userid):
